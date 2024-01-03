0c996f79f91a4fcd964df92bbe2fc7bfb180da3e
johnasblasco <johnaslblasco@gmail.com>
2024-01-04 00:48:26 +0800

    main

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB 3.0 | Charity PH</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+    <style>
+        #terms a, #info a, footer p {
+            color: #535d66;
+            text-decoration: none;
+        }
+        #terms a:hover, #info a:hover{
+            color: #a5afb8;
+        }
+        .navbar-brand:hover{
+            color: #00a8e8;
+        }
+        footer{
+            background-color: #F3F8FF;
+        }
+    </style>
+</head>
+
+<body>
+<header>
+    <nav class="navbar fixed-top navbar-light bg-light mb-5">
+        <div class="container-fluid d-flex justify-content-around align-items-center">
+            <img src="assets/PH.png" alt="IMAGE HERE" class = "img-fluid" style = "width : 80px; height : 80px">
+            <div class="d-flex justify-content-center">
+                <a class="navbar-brand fs-6" href="#">About</a>
+                <a class="navbar-brand fs-6" href="#">Donate</a>
+                <a class="navbar-brand fs-6" href="#">Blog</a>
+            </div>
+            <button class="btn btn-primary">Donate</button>
+        </div>
+    </nav>
+</header>
+
+<main>
+    <section class="mt-5 pt-5">
+        <div class="container mt-5">
+            <div class="container row">
+                <div class="col-md-6">
+                    <h1>The Charity of the Philippines</h1>
+                    <div class="card">
+                    <div class="row g-0">
+                        <div class="col-5 col-sm-4">
+                        <img src="assets/images/bs-images/img-3x4.png" class="img-fluid w-100" alt="card-horizontal-image">
+                        </div>
+                        <div class="col-7 col-sm-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+
+
+                </div>
+
+                <!-- RIGHT -->
+                <div class="col-md-6">
+                   <img src="assets/COTP.png" alt="img" style = "width : 100vh">
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<footer class="footer text-center m-2 p-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+        <img src="assets/PH.png" alt="" style="width: 100px; height: 100px">
+                <p>
+                    <a href="https://cotp.com.ph">© 2024 Charity Of The Philippines</a> |
+                    <a href="https://cotp.com.ph">cotp.com.ph</a> | All rights Reserved.
+                </p>
+            </div>
+            <div class="col-lg-4 mt-2">
+                <h5>Contact Us</h5>
+                <p>123 Main Street, City</p>
+                <p>Email: <a href="mailto:info@example.com">info@example.com</a></p>
+                <p>Phone: <a href="tel:1234567890">123-456-7890</a></p>
+            </div>
+
+            <div class="col-lg-2 mt-3" id = "info">
+                <h5>Info</h5>
+                <p><a href="#">About Us</a></p>
+                <p><a href="#">Blog</a></p>
+            </div>
+
+            <div class="col-lg-2 mt-3" id = "terms">
+                <p><a href="https://www.termsfeed.com/live/7b24c2e5-6216-4276-a3c2-913a5b6b38bf">Privacy Policy</a></p>
+                <p><a href="https://www.termsfeed.com/live/7b24c2e5-6216-4276-a3c2-913a5b6b38bf">Terms & Conditions</a></p>
+            </div>
+
+      </div>
+    </div>
+  </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
